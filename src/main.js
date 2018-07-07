7377ff360d1a0c4eb72c844bbb05b586613ef753
LuoDan <1178886976@qq.com>
2018-07-07 16:21:32 +0800

    开发axios插件,并进行引用

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ import router from './routes/router'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 //3 将本身的css 放到assets 中 并引入
 import './assets/css/index.css'
-//导入axios 
-import axios from 'axios';
-// 配置baseurl
-axios.defaults.baseURL ='http://localhost:3000/';
-Vue.prototype.$http = axios;
+// 导入自定义插件
+import MyAxios from'./plugins/myaxios.js'
+//注册插件
+Vue.use(MyAxios);
+
 
 new Vue({
   el: '#app',
